2ef76f6f7dbcfe8ad922f882d3c1df27dc47bb1b
FPVian <iangresov@gmail.com>
2023-06-27 21:22:05 -0400

    fixed dependencies

--- deployment/pulumi.py
+++ deployment/pulumi.py
@@ -1,7 +1,99 @@
 from flights.config.settings import s
 
 import pulumi
-from pulumi_azure_native import resources
+from pulumi_azure_native import resources, network, compute, app
 
-# Create an Azure Resource Group
+'''
+Pulumi Azure Native API docs: https://www.pulumi.com/registry/packages/azure-native/api-docs/
+'''
+
 resource_group = resources.ResourceGroup("sky-viz", location='eastus')
+
+## Key Vault
+## Azure Container Registry?
+
+## Virtual Network
+net = network.VirtualNetwork(
+    "server-network",
+    resource_group_name=resource_group.name,
+    address_space=network.AddressSpaceArgs(
+        address_prefixes=["10.0.0.0/16"],
+    ),
+    subnets=[network.SubnetArgs(
+        name="default",
+        address_prefix="10.0.1.0/24",
+    )])
+
+
+## Virtual Machine
+vm = compute.VirtualMachine(
+    "flights-vm",
+    resource_group_name=resource_group.name,
+    network_profile=compute.NetworkProfileArgs(
+        network_interfaces=[
+            compute.NetworkInterfaceReferenceArgs(id=network_iface.id),
+        ],
+    ),
+    hardware_profile=compute.HardwareProfileArgs(
+        vm_size=compute.VirtualMachineSizeTypes.STANDARD_B1S,
+    ),
+    os_profile=compute.OSProfileArgs(
+        computer_name="hostname",
+        admin_username=username,
+        admin_password=password,
+        custom_data=base64.b64encode(init_script.encode("ascii")).decode("ascii"),
+        linux_configuration=compute.LinuxConfigurationArgs(
+            disable_password_authentication=False,
+        ),
+    ),
+    storage_profile=compute.StorageProfileArgs(
+        os_disk=compute.OSDiskArgs(
+            create_option=compute.DiskCreateOptionTypes.FROM_IMAGE,
+            name="myosdisk1",
+        ),
+        image_reference=compute.ImageReferenceArgs(
+            publisher="canonical",
+            offer="UbuntuServer",
+            sku="22.04-LTS",
+            version="latest",
+        ),
+    ))
+
+## install postgres
+
+## Virtual Network
+
+## Static IP
+public_ip = network.PublicIPAddress(
+    "skyviz.app-ip",
+    resource_group_name=resource_group.name,
+    public_ip_allocation_method=network.IPAllocationMethod.STATIC)
+
+
+## Container App - certificate
+container_app = app.ContainerApp("app",
+    resource_group_name=resource_group.name,
+    managed_environment_id=managed_env.id,
+    configuration=app.ConfigurationArgs(
+        ingress=app.IngressArgs(
+            external=True,
+            target_port=80
+        ),
+        registries=[
+            app.RegistryCredentialsArgs(
+                server=registry.login_server,
+                username=admin_username,
+                password_secret_ref="pwd")
+        ],
+        secrets=[
+            app.SecretArgs(
+                name="pwd",
+                value=admin_password)
+        ],
+    ),
+    template=app.TemplateArgs(
+        containers = [
+            app.ContainerArgs(
+                name="myapp",
+                image=my_image.image_name)
+        ]))
